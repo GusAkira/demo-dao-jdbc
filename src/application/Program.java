e371a9837d2b7cb4f27fff81e0cef00560a2ac69
GusAkira <gustavoakira19@gmail.com>
2025-07-10 20:05:55 -0300

    findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,6 @@
 package application;
 
-import java.util.Date;
+import java.util.List;
 import java.util.Scanner;
 
 import model.dao.DaoFactory;
@@ -12,20 +12,30 @@ public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Scanner sc = new Scanner(System.in);
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+
 		System.out.println("====  test1: seller findById ====");
-		
+
 		System.out.print("Digite um ID: ");
-		int i = sc.nextInt();
-
-		System.out.print("ID localizado: ");
-		Seller seller = sellerDao.findById(i);
-
+		int id = sc.nextInt();
+		System.out.println("ID localizado: ");
+		Seller seller = sellerDao.findById(id);
 		System.out.println(seller);
+		System.out.println();
+
+		System.out.println("====  test2: seller findByDepartment ====");
+		
+		System.out.print("Digite um Departamento: ");
+		int dep = sc.nextInt();
+		System.out.println("Departamento localizado: ");
+		Department department = new Department(dep,null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 
 	}
 
