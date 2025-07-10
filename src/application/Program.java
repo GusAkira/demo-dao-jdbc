db60fb03aadf63cd443e90e87e20c738012de18e
GusAkira <gustavoakira19@gmail.com>
2025-07-10 16:05:34 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,16 +11,13 @@ public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		Department obj = new Department(1,"Books");
-		
-		Seller seller = new Seller(21,"bob","bob@gmail.com", new Date(),3000.0,obj);
-		
+
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
-		System.out.println(obj);
+
+		Seller seller = sellerDao.findById(2);
+
 		System.out.println(seller);
-		
+
 	}
 
 }
--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,6 @@
  * 
  */
 module demoDaoJdbc {
+	requires java.sql;
+	
 }
